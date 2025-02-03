3447ca176bb1daf234488fb92da31d905146795e
TaYaKi71751 <shantian.tw@gmail.com>
2025-02-03 12:03:45 +0900

    First commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while true
+do
+ SERVER_PIDS="$(ps ax | grep server.jar | cut -d ' ' -f1)"
+	while IFS= read -r SERVER_PID
+	do
+		kill -9 $SERVER_PID
+	done < <(printf '%s\n' "${SERVER_PIDS}")
+	rm server.jar
+	JAR_URL="$(curl --http1.1 -A "Mozilla/5.0" -LsSf "https://www.minecraft.net/en-us/download/server" | grep server.jar | rev | cut -d '"' -f4 | rev)"
+	echo $JAR_URL
+	curl --http1.1 -A "Mozilla/5.0" -LsSf "${JAR_URL}" -o server.jar
+	echo "eula=true" > eula.txt
+	java -jar server.jar --nogui &
+	sleep 3600
+done
